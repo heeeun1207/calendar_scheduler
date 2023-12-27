8639866a3279d78c42264b8dfb66339321cd3a30
jungheeeun <jungheeeun272@gmai.com>
2023-12-27 16:32:49 +0900

    달력 확인 ✔

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:calendar_scheduler/component/main_calendar.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,9 +7,19 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Text('Home Screen!!!'),
+      body: SafeArea( // 시스템 UI 피해서 UI 구현하기 위해서
+        child: Column( // 달력과 리스트를 새로로 배치
+          children: [
+            // 미리 작업해둔 달력 위젯 불러오기
+            MainCalendar(),
+          ],
+        ),
+      )
     );
   }
 }
+
+
+
 
 
