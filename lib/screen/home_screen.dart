e7f6ba30b5d408168feb6ef2132a7930e029fc0c
jungheeeun <jungheeeun272@gmai.com>
2023-12-27 17:24:57 +0900

    상태추가 : 날짜 선택되도록 스크린에 구현

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,25 +1,45 @@
 import 'package:calendar_scheduler/component/main_calendar.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  //1. StatefulWidget 변경
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDate = DateTime.utc( // 2.선택된 날짜를 관리할 변수
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea( // 시스템 UI 피해서 UI 구현하기 위해서
-        child: Column( // 달력과 리스트를 새로로 배치
+      body: SafeArea(
+        child: Column(
           children: [
-            // 미리 작업해둔 달력 위젯 불러오기
-            MainCalendar(),
+            MainCalendar(
+              // 선택된 날짜를 MainCalendar에 전달
+              selectedDate: selectedDate,
+
+              // 날짜가 선택됐을 때 실행할 함수
+              onDaySelected: onDaySelected,
+            ),
           ],
         ),
-      )
+      ),
     );
   }
+
+  // 3.날짜가 선택됐을 때 실행되는 함수
+  void onDaySelected(DateTime selectedDate, DateTime focusedDate) {
+    // 선택된 날짜 업데이트
+    setState(() {
+      this.selectedDate = selectedDate;
+    });
+  }
 }
-
-
-
-
-
