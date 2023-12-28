f1caa76d37aeb516b331bcd03236c1d223f299ac
jungheeeun <jungheeeun272@gmai.com>
2023-12-28 17:01:32 +0900

    임시 일정 시간,내용 적용 확인

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:calendar_scheduler/component/main_calendar.dart';
 import 'package:flutter/material.dart';
+import 'package:calendar_scheduler/component/schedule_card.dart';
 
 class HomeScreen extends StatefulWidget {
   //1. StatefulWidget 변경
@@ -28,6 +29,11 @@ class _HomeScreenState extends State<HomeScreen> {
 
               // 날짜가 선택됐을 때 실행할 함수
               onDaySelected: onDaySelected,
+            ),
+            ScheduleCard(
+                startTime: 12,
+                endTime: 14,
+                content: '프로그래밍 공부',
             ),
           ],
         ),
