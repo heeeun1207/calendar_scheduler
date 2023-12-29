c35fc1ed734afb17668f2c1904804291be1a407c
jungheeeun <jungheeeun272@gmai.com>
2023-12-29 16:36:27 +0900

    일정 안에 필드 구현, 스타일추가

--- lib/component/custom_text_field.dart
+++ lib/component/custom_text_field.dart
@@ -1,11 +1,14 @@
 import 'package:calendar_scheduler/const/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CustomTextField extends StatelessWidget {
-  final String label;
+  final String label; // 택스트 필드 제목
+  final bool isTime; // 시간 선택하는 텍스트 필드인지 여부
 
   const CustomTextField({
     required this.label,
+    required this.isTime,
     Key? key,
 }) : super(key: key);
 
@@ -21,13 +24,44 @@ class CustomTextField extends StatelessWidget {
             fontWeight: FontWeight.w600,
           ),
         ),
-        TextFormField(), // 폼 안에서 텍스트 필드를 쓸 때 사용
+        Expanded(
+            flex: isTime ? 0 : 1, // 1
+            child:TextFormField( //폼 안에서 텍스트 필드를 쓸 때 사용
+              cursorColor: Colors.grey, // 커서 색상 변경
+              maxLines: isTime? 1 : null,
+              // 시간 관련 텍스트 필드가 아니면 한 줄 이상 작성 가능하게
+              expands: !isTime, // 시간 관련 텍스트 필드는 공간 최대 차지하도록
+              keyboardType: isTime ? TextInputType.number : TextInputType.multiline,
+              // 시간 관련 텍스트 필드는 기본 숫자 키보드로 아닐 경우에 일반 글자 키보드 보여주기
+              inputFormatters: isTime
+                ? [
+               FilteringTextInputFormatter.digitsOnly,
+              ]
+                  : [], // 시간 관련 텍스트 필드는 숫자만 입력하도록 제한
+              decoration: InputDecoration(
+                border: InputBorder.none, // 테두리 삭제
+                filled: true, // 배경색 지정하겠다는 선언
+                fillColor: Colors.grey[300], // 배경색
+                suffixText: isTime ? '시' : null,
+                // 시간 관련 텍스트 필드는 '시' 접미사 추가
+              ),
+            ),
+        ),
       ],
     );
   }
 }
 
-// 플러터에서 TextField 위젯과 TextFormField 로 나눌 수 있다.
-// TextField 위젯은 각 텍스트 필드가 독립된 형태일 때 주로 사용한다.
-// TextFormField 는 여러개의 텍스트 필드를 하나의 폼으로 제어할 때 사용한다.
-// 저장 버튼 눌렀을 때 필드 2개와 텍스트 필드 하나를 제어할 계획이므로, TextFormField 를 사용한다.
+// maxLines 매개변수 : 텍스트 필드에 값을 입력할 때 허락되는 최대 줄 개수임. int 값을 입력할 수있으며,
+// null 을 넣으면 개수를 제한하지 않는다.
+
+// expands 매개변수 : 기본값 false 이며 true 로 입력하면 부모의 위젯 크기만큼 텍스트 필드를 늘릴 수 있음.
+
+// TextInputType .multiline  : 줄바꿈 키가 존재하는 일반 키보드
+
+// inputFormatters 키보드 종류를 정의하는 keyboardType 비슷하지만 큰 차이가 있다.
+// keyboardType 는  오직 핸드폰 키보드만 제한할 수 있다.
+// inputFormatters 는 특정 입력 자체를 제한할 수 있다
+// 예시 : 블루투스, 보안키보드와 같은 커스텀 구현된 키보드
+
+// suffixText : 접미사를 직접 지정한다. (오른쪽 끝에 원하는 글자가 상시 표시됨)
--- lib/component/schedule_bottom_sheet.dart
+++ lib/component/schedule_bottom_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:calendar_scheduler/component/custom_text_field.dart';
+import 'package:calendar_scheduler/const/colors.dart';
 import 'package:flutter/material.dart';
 
 class ScheduleBottomSheet extends StatefulWidget {
@@ -15,12 +16,55 @@ class _ScheduleBottomSheet extends State<ScheduleBottomSheet> {
         child: Container(
           height: MediaQuery.of(context).size.height / 2,
           color: Colors.white,
-          child: CustomTextField(
-            // 시작 시간 텍스트 필드 렌더링
-            label: '시작 시간',
+          child: Padding(
+            padding: const EdgeInsets.only(left: 8,right: 8,top: 8),
+            child: Column(
+              // 시간 관련 텍스트 필드와 내용 관련 텍스트 필드 세로로 배치
+              children: [
+                Row(
+                  // 시작 시간 종료 시간 가로로 배치
+                  children: [
+                    Expanded(
+                        child: CustomTextField( // 시작 시간 입력 필드
+                          label: '시작 시간',
+                          isTime: true,
+                        ),
+                    ),
+                    const SizedBox(width: 16.0),
+                    Expanded(
+                        child: CustomTextField( // 종료 시간 입력 필드
+                            label: '종료  시간',
+                            isTime: true,
+                        ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 8.0),
+                Expanded(
+                    child: CustomTextField( // 내용 입력 필드
+                      label: '내용',
+                      isTime: false,
+                    ),
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton( // [저장] 버튼
+                    onPressed: onSavePressed,
+                    style: ElevatedButton.styleFrom(
+                      primary: PRIMARY_COLOR,
+                    ),
+                    child: Text('저장'),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
     );
+  }
+
+  void onSavePressed() {
+    // 임시
   }
 }
 
