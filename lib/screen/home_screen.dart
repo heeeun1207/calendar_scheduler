cdf3dd5cf2ef0c2b89ef424982cc32e744f3fe80
jungheeeun <jungheeeun272@gmai.com>
2024-01-01 20:29:37 +0900

    선택한 날짜에 맞는 일정 데이터 읽고 순서대로 저장하기

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -4,10 +4,11 @@ import 'package:calendar_scheduler/const/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:calendar_scheduler/component/schedule_card.dart';
 import 'package:calendar_scheduler/component/today_banner.dart';
-
+import 'package:get_it/get_it.dart';
+import 'package:calendar_scheduler/database/drift_database.dart';
 
 class HomeScreen extends StatefulWidget {
-  //1. StatefulWidget 변경
+  // StatefulWidget 변경
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
@@ -15,7 +16,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  DateTime selectedDate = DateTime.utc( // 2.선택된 날짜를 관리할 변수
+  DateTime selectedDate = DateTime.utc( // 선택된 날짜를 관리할 변수
     DateTime.now().year,
     DateTime.now().month,
     DateTime.now().day,
@@ -58,18 +59,44 @@ class _HomeScreenState extends State<HomeScreen> {
                 count: 0 // 임시
             ),
             SizedBox(height: 8.0),
-            ScheduleCard(
-                startTime: 12,
-                endTime: 14,
-                content: '프로그래밍 공부',
+            Expanded( // 남는 공간을 모두 차지하기
+              // 일정 정보가 Stream으로 제공되기 때문에 StreamBuilder 사용한다
+                child: StreamBuilder<List<Schedule>>(
+                  stream: GetIt.I<LocalDatabase>().watchSchedules(selectedDate),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) { // 데이터가 없을 때
+                      return Container();
+                    }
+                    // 화면에 보이는 값들만 렌더링하는 리스트
+                    return ListView.builder(
+                      // 리스트에 입력할 값들의 총 개수
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, index) {
+                        // 현재 index에 해당되는 일정
+                        final schedule = snapshot.data![index];
+                        return Padding( // 좌우로 패딩을 추가해서 UI 개선
+                          padding: const EdgeInsets.only(
+                              bottom: 8.0, left: 8.0, right: 8.0),
+                          child: ScheduleCard(
+                            startTime: schedule.startTime,
+                            endTime: schedule.endTime,
+                            content: schedule.content,
+                          ),
+                        );
+                      },
+                    );
+                  }
+                ),
+              // Stream 을 사용하면 데이터를 일회성으로 조회하는게 아니라
+              // 지속적으로 변화가 있을 때 새로운 값들을 받아올 수 있다.
             ),
-          ],
+            ],
+            ),
         ),
-      ),
     );
   }
 
-  // 3.날짜가 선택됐을 때 실행되는 함수
+  // 날짜가 선택됐을 때 실행되는 함수
   void onDaySelected(DateTime selectedDate, DateTime focusedDate) {
     // 선택된 날짜 업데이트
     setState(() {
