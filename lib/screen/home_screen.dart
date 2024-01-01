fb9a02bd2c7610dbb8dc62f30eac1e84cd964b01
jungheeeun <jungheeeun272@gmai.com>
2024-01-01 18:31:38 +0900

    HomeScreen 에 selectedDate 변수 입력받기

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -30,7 +30,9 @@ class _HomeScreenState extends State<HomeScreen> {
           showModalBottomSheet( // BottomSheet 열기
               context: context,
               isDismissible: true, // 배경을 탭했을 때  BottomSheet 닫기
-              builder: (_) => ScheduleBottomSheet(),
+              builder: (_) => ScheduleBottomSheet(
+                selectedDate: selectedDate, // 선택된  날짜 (selectedDate) 넘겨주기
+              ),
             // BottomSheet 높이를 화면 최대 높이로
             // 정의하고 스크롤 가능하게 변경해준다.
             isScrollControlled: true,
