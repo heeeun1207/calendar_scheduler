3653333ec0c4a46aac4d99a81a23dea320c991a1
jungheeeun <jungheeeun272@gmai.com>
2024-01-08 18:34:47 +0900

    ScheduleProvider 클래스 선언

---
+++ lib/provider/schedule_provider.dart
@@ -0,0 +1,22 @@
+import 'package:calendar_scheduler/model/schedule_model.dart';
+import 'package:calendar_scheduler/repository/schedule_repository.dart';
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ScheduleProvider extends ChangeNotifier { //ChangeNotifier: material 패키지에서 제공
+  final ScheduleRepository repository; // API 요청 로직을 담은 클래스
+
+DateTime selectedDate = DateTime.utc( // 선택한 날짜
+  DateTime.now().year,
+  DateTime.now().month,
+  DateTime.now().day,
+);
+Map<DateTime, List<ScheduleModel>> cache = {}; // 일정 정보를 저장해둘 변수
+
+ScheduleProvider({
+    required this.repository,
+}) : super() {
+  getSchedules(date: selectedDate);
+}
+}
