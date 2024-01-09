8c33d03f2911c6365b351cbf5e8df5a6f7a8a3fc
jungheeeun <jungheeeun272@gmai.com>
2024-01-09 16:05:39 +0900

    드리프트에서 사용한 StreamBuilder 삭제하기

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -4,20 +4,12 @@ import 'package:calendar_scheduler/const/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:calendar_scheduler/component/schedule_card.dart';
 import 'package:calendar_scheduler/component/today_banner.dart';
-import 'package:get_it/get_it.dart';
-import 'package:calendar_scheduler/database/drift_database.dart';
 
 // Provider 불러오기
 import 'package:provider/provider.dart';
 import 'package:calendar_scheduler/provider/schedule_provider.dart';
 
-
 class HomeScreen extends StatelessWidget{
-  DateTime selectedDate = DateTime.utc( // 선택된 날짜를 관리할 변수
-    DateTime.now().year,
-    DateTime.now().month,
-    DateTime.now().day,
-  );
 
   @override
   Widget build(BuildContext context) {
@@ -61,55 +53,36 @@ class HomeScreen extends StatelessWidget{
               onDaySelected: onDaySelected,
             ),
             SizedBox(height: 8.0),
-            StreamBuilder<List<Schedule>>( // 일정 Stream 으로 받아오기
-              stream: GetIt.I<LocalDatabase>().watchSchedules(selectedDate),
-              builder: (context, snapshot) {
-                return TodayBanner(
-                    selectedDate: selectedDate,
-                    count: snapshot.data?.length ?? 0, // 일정 개수 입력해주기
-                );
-              },
+            TodayBanner(
+                selectedDate: selectedDate,
+                count: schedules.length,
             ),
             SizedBox(height: 8.0),
-            Expanded( // 남는 공간을 모두 차지하기
-              // 일정 정보가 Stream으로 제공되기 때문에 StreamBuilder 사용한다
-                child: StreamBuilder<List<Schedule>>(
-                  stream: GetIt.I<LocalDatabase>().watchSchedules(selectedDate),
-                  builder: (context, snapshot) {
-                    if (!snapshot.hasData) { // 데이터가 없을 때
-                      return Container();
-                    }
-                    // 화면에 보이는 값들만 렌더링하는 리스트
-                    return ListView.builder(
-                      // 리스트에 입력할 값들의 총 개수
-                      itemCount: snapshot.data!.length,
-                      itemBuilder: (context, index) {
-                        // 현재 index에 해당되는 일정
-                        final schedule = snapshot.data![index];
-                        return Dismissible(
-                            key: ObjectKey(schedule.id), // 유니크 키값
-                            // 밀기 방향(오른쪽으로 왼쪽으로)
-                            direction: DismissDirection.startToEnd,// endToStart가 왼손잡이한테 더 편한것 같은데,, ✔🤷‍♀️
-                            // 밀기 했을 때 실행할 함수
-                          onDismissed: (_) { // (  onDismissed: (DismissDirection.direction)  에서 수정함,,
-                            GetIt.I<LocalDatabase>().removeSchedule(schedule.id);
-                          },
-                          child : Padding( // 좌우로 패딩을 추가해서 UI 개선
-                            padding: const EdgeInsets.only(
-                              bottom: 8.0, left: 8.0, right: 8.0),
-                            child: ScheduleCard(
-                              startTime: schedule.startTime,
-                              endTime: schedule.endTime,
-                              content: schedule.content,
+
+            Expanded(
+                child: ListView.builder(
+                  itemCount: schedules.length,
+                  itemBuilder: (count, index) {
+                    final schedule = schedules[index];
+
+                    return Dismissible(
+                        key: ObjectKey(schedule.id),
+                        direction: DismissDirection.startToEnd,
+                        onDismissed: (DismissDirection direction) {
+                          provider.deleteSchedule(date: selectedDate, id: schedule.id);
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                            bottom: 8.0, left: 8.0, right: 8.0),
+                          child: ScheduleCard(
+                            startTime: schedule.startTime,
+                            endTime: schedule.endTime,
+                            content: schedule.content,
                           ),
-                          ),
-                        );
-                      },
+                        ),
                     );
-                  }
+                  },
                 ),
-              // Stream 을 사용하면 데이터를 일회성으로 조회하는게 아니라
-              // 지속적으로 변화가 있을 때 새로운 값들을 받아올 수 있다.
             ),
             ],
             ),
