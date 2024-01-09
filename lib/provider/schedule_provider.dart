56d58b182aaf99a16bfdad135fb117b1a685ca2c
jungheeeun <jungheeeun272@gmai.com>
2024-01-09 17:01:52 +0900

    일정을 생성하는 작업에 긍정적 응답 적용해보기

--- lib/provider/schedule_provider.dart
+++ lib/provider/schedule_provider.dart
@@ -2,83 +2,99 @@ import 'package:calendar_scheduler/model/schedule_model.dart';
 import 'package:calendar_scheduler/repository/schedule_repository.dart';
 
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+// import 'package:provider/provider.dart';
+import 'package:uuid/uuid.dart';
 
-class ScheduleProvider extends ChangeNotifier { //ChangeNotifier: material 패키지에서 제공
+class ScheduleProvider extends ChangeNotifier {
+  //ChangeNotifier: material 패키지에서 제공
   final ScheduleRepository repository; // API 요청 로직을 담은 클래스
 
-DateTime selectedDate = DateTime.utc( // 선택한 날짜
-  DateTime.now().year,
-  DateTime.now().month,
-  DateTime.now().day,
-);
-Map<DateTime, List<ScheduleModel>> cache = {}; // 일정 정보를 저장해둘 변수
+  DateTime selectedDate = DateTime.utc( // 선택한 날짜
+    DateTime
+        .now()
+        .year,
+    DateTime
+        .now()
+        .month,
+    DateTime
+        .now()
+        .day,
+  );
+  Map<DateTime, List<ScheduleModel>> cache = {}; // 일정 정보를 저장해둘 변수
 
-ScheduleProvider({
+  ScheduleProvider({
     required this.repository,
-}) : super() {
-  getSchedules(date: selectedDate);
-}
-
-void getSchedules({
-    required DateTime date,
-}) async {
-  final resp = await repository.getSchedules(date: date); // GET 메서드 보내기
-
-  // 선택한 날짜의 일정들 업데이트하기
-  cache.update(date, (value) => resp, ifAbsent: () => resp);
-
-  notifyListeners(); // 리슨하는 위젯들 업데이트하기
+  }) : super() {
+    getSchedules(date: selectedDate);
   }
-  
+
+  void getSchedules({
+    required DateTime date,
+  }) async {
+    final resp = await repository.getSchedules(date: date); // GET 메서드 보내기
+
+    // 선택한 날짜의 일정들 업데이트하기
+    cache.update(date, (value) => resp, ifAbsent: () => resp);
+
+    notifyListeners(); // 리슨하는 위젯들 업데이트하기
+  }
+
   void createSchedule({
     required ScheduleModel schedule,
-})async {
-  final targetDate = schedule.date;
-  
-  final savedSchedule = await repository.createSchedule(schedule: schedule);
-  
-  cache.update(
+  }) async {
+    final targetDate = schedule.date;
+
+    final uuid = Uuid();
+
+    final tempId = uuid.v4(); // 유일한 ID 값을 생성한다.
+    final newSchedule = schedule.copyWith(
+      id: tempId, // 임시 ID를 저장한다
+    );
+
+    // 긍정적 응답구간이다. 서버에서 응답을 받기 전에 캐시를 먼저 업데이트 해준다
+    cache.update(
       targetDate,
-          (value) => [ // 현존하는 캐시 리스트 끝에 새로운 일정 추가
-            ...value,
-            schedule.copyWith(
-              id: savedSchedule,
-            ),
-          ]..sort(
-              (a,b) => a.startTime.compareTo(
-                b.startTime,  // 시작 시간을 비교해서 오름차순 정렬
+          (value) =>
+      [
+        ...value,
+        newSchedule,
+      ]
+        ..sort(
+              (a, b) =>
+              a.startTime.compareTo(
+                b.startTime,
               ),
-          ),
-    // 날짜에 해당하는 값이 없다면 새로운 리스트에 새로운 일정 하나만 추가
-    ifAbsent: () => [schedule],
-  );
+        ),
+      ifAbsent: () => [newSchedule],
+    );
 
-  notifyListeners();
-  }
+    notifyListeners(); // 캐시 업데이트 반영하기
 
-  void deleteSchedule({
-    required DateTime date,
-    required String id,
-}) async {
-  final resp = await repository.deleteSchedule(id: id);
+    try {
+      // API 요청하기
+      final savedSchedule = await repository.createSchedule(schedule: schedule);
 
-  cache.update( // 캐시에서 데이터 삭제
-      date,
-          (value) => value.where((e) => e.id != id).toList(),
-  ifAbsent: () => [],
-  );
+      cache.update( // 서버 응답 기반으로 캐시 업데이트
+        targetDate,
+            (value) =>
+            value
+                .map((e) =>
+            e.id == tempId
+                ? e.copyWith(
+              id: savedSchedule,
+            )
+                : e)
+                .toList(),
+      );
+    } catch (e) {
+      cache.update( // 삭제 실패 시 캐시 롤백하기
+        targetDate,
+            (value) => value.where((e) => e.id != tempId).toList(),
+      );
+    }
 
-  notifyListeners();
-  }
-
-  void changeSelectedDate({
-    required DateTime date,
-}) {
-  //단순히 매개변수로 입력받은 날짜 변수로 selectedDate 변수를 대체하는 코드를 작성하면 끝
-  selectedDate = date; // 현재 선택된 날짜를 매개변수로 입력받은 날짜로 변경
     notifyListeners();
   }
 }
 
-
+  
